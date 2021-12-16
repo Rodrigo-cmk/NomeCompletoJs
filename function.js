27ea64ec1320a1fe82f114984bae3897ef3703b9
Rodrigo-cmk <rodrigorac.2003@gmail.com>
2021-12-16 20:22:39 -0300

    Finalizando Exercício

---
+++ function.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu Nome: ");
+let sobrenome = prompt("Digite seu Sobrenome: ");
+
+alert ("Seu nome completo é: " + nome + " " + sobrenome);
+
+alert ("Seu nome de catálogo é: " + sobrenome.toUpperCase() + ", " + nome);
